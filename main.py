d316289dacdb63c244b1c2c8a226d004fedcc654
Robin Michael <Siliconrob@siliconheaven.net>
2023-06-07 22:46:43 -0500

    Addinomicon

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
+import datetime
 import os
 import httpx
 from dataclasses import dataclass
 from furl import furl
+import arrow
 
 
 @dataclass
@@ -41,9 +43,31 @@ async def get_authorized_async(request_url: furl, auth_user: OwnerRezApiUser) ->
         r.raise_for_status()
         return r.json()
 
+
+def default_start_date() -> str:
+    return arrow.get(datetime.date(2000, 1, 1)).to('utc').format("YYYY-MM-DD")
+
+
 async def async_main():
-    print(get_authorized(furl(API_URL).set(path=f'v2/fielddefinitions'), get_user()))
-    print(await get_authorized_async(furl(API_URL).set(path=f'v2/fielddefinitions'), get_user()))
+    user = get_user()
+    in_the_beginning_date = default_start_date()
+    id = 1
+
+    # print(get_authorized(furl(API_URL).set(path=f'v2/fielddefinitions'), get_user()))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/bookings', args=dict(since_utc=in_the_beginning_date)), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/fielddefinitions'), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/fields', args=dict(entity_type='property', entity_id=id)), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/guests', args=dict(created_since_utc=in_the_beginning_date)), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/inquiries', args=dict(since_utc=in_the_beginning_date)), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/listings'), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/owners'), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/properties'), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/quotes', args=dict(created_since_utc=in_the_beginning_date)), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/tagdefinitions', args=dict(active=True)), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/users/me'), user))
+    print(await get_authorized_async(furl(API_URL).set(path=f'v2/webhooksubscriptions'), user))
+
 
 if __name__ == '__main__':
-     asyncio.run(async_main())
+    asyncio.run(async_main())
+
