0ae0944a4fb14268713a7696e5744818f56eb0e5
Robin Michael <Siliconrob@siliconheaven.net>
2023-06-08 09:26:24 -0500

    Naming fixes

--- main.py
+++ main.py
@@ -12,26 +12,34 @@ def default_start_date() -> str:
     return arrow.get(datetime.date(2000, 1, 1)).to('utc').format("YYYY-MM-DD")
 
 
+def run_gets(start_date: str, start_id: int, input_user: orez.OwnerRezApiUser):
+    print(api.get(furl(API_URL).set(path=f'v2/bookings', args=dict(since_utc=start_date)), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/fielddefinitions'), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/fields', args=dict(entity_type='property', entity_id=start_id)), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/guests', args=dict(created_since_utc=start_date)), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/inquiries', args=dict(since_utc=start_date)), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/listings'), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/owners'), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/properties'), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/quotes', args=dict(created_since_utc=start_date)), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/tagdefinitions', args=dict(active=True)), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/users/me'), input_user))
+    print(api.get(furl(API_URL).set(path=f'v2/webhooksubscriptions'), input_user))
+
+
 async def run_gets_async(start_date: str, start_id: int, input_user: orez.OwnerRezApiUser):
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/bookings', args=dict(since_utc=start_date)),
-                                         input_user))
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/fielddefinitions'), input_user))
-    print(await api.get_authorized_async(
-        furl(API_URL).set(path=f'v2/fields', args=dict(entity_type='property', entity_id=start_id)), input_user))
-    print(await api.get_authorized_async(
-        furl(API_URL).set(path=f'v2/guests', args=dict(created_since_utc=start_date)), input_user))
-    print(
-        await api.get_authorized_async(furl(API_URL).set(path=f'v2/inquiries', args=dict(since_utc=start_date)),
-                                       input_user))
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/listings'), input_user))
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/owners'), input_user))
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/properties'), input_user))
-    print(await api.get_authorized_async(
-        furl(API_URL).set(path=f'v2/quotes', args=dict(created_since_utc=start_date)), input_user))
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/tagdefinitions', args=dict(active=True)),
-                                         input_user))
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/users/me'), input_user))
-    print(await api.get_authorized_async(furl(API_URL).set(path=f'v2/webhooksubscriptions'), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/bookings', args=dict(since_utc=start_date)), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/fielddefinitions'), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/fields', args=dict(entity_type='property', entity_id=start_id)), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/guests', args=dict(created_since_utc=start_date)), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/inquiries', args=dict(since_utc=start_date)), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/listings'), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/owners'), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/properties'), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/quotes', args=dict(created_since_utc=start_date)), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/tagdefinitions', args=dict(active=True)), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/users/me'), input_user))
+    print(await api.get_async(furl(API_URL).set(path=f'v2/webhooksubscriptions'), input_user))
 
 
 async def async_main():
--- template_requests.py
+++ template_requests.py
@@ -3,42 +3,70 @@ from furl import furl
 import owner_rez_api_user as orez
 
 
-def get_authorized(request_url: furl, auth_user: orez.OwnerRezApiUser) -> dict:
+def get(request_url: furl, auth_user: orez.OwnerRezApiUser) -> dict:
     with httpx.Client(headers=auth_user.headers()) as client:
         r = client.get(request_url.url, auth=auth_user.get_auth())
         r.raise_for_status()
         return r.json()
 
 
-async def get_authorized_async(request_url: furl, auth_user: orez.OwnerRezApiUser) -> dict:
+async def get_async(request_url: furl, auth_user: orez.OwnerRezApiUser) -> dict:
     async with httpx.AsyncClient(headers=auth_user.headers()) as client:
         r = await client.get(request_url.url, auth=auth_user.get_auth())
         r.raise_for_status()
         return r.json()
 
 
-async def post_authorized_async(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
+async def post(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
+    with httpx.Client(headers=auth_user.headers()) as client:
+        r = client.post(request_url.url, data=send_data, auth=auth_user.get_auth())
+        r.raise_for_status()
+        return r.json()
+
+
+async def post_async(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
     async with httpx.AsyncClient(headers=auth_user.headers()) as client:
         r = await client.post(request_url.url, data=send_data, auth=auth_user.get_auth())
         r.raise_for_status()
         return r.json()
 
 
-async def patch_authorized_async(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
+async def patch(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
     async with httpx.AsyncClient(headers=auth_user.headers()) as client:
         r = await client.patch(request_url.url, data=send_data, auth=auth_user.get_auth())
         r.raise_for_status()
         return r.json()
 
 
-async def put_authorized_async(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
+async def patch_async(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
+    async with httpx.AsyncClient(headers=auth_user.headers()) as client:
+        r = await client.patch(request_url.url, data=send_data, auth=auth_user.get_auth())
+        r.raise_for_status()
+        return r.json()
+
+
+def put(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
+    with httpx.Client(headers=auth_user.headers()) as client:
+        r = client.put(request_url.url, data=send_data, auth=auth_user.get_auth())
+        r.raise_for_status()
+        return r.json()
+
+
+async def put_async(request_url: furl, send_data: dict, auth_user: orez.OwnerRezApiUser) -> dict:
     async with httpx.AsyncClient(headers=auth_user.headers()) as client:
         r = await client.put(request_url.url, data=send_data, auth=auth_user.get_auth())
         r.raise_for_status()
         return r.json()
 
 
-async def delete_authorized_async(request_url: furl, auth_user: orez.OwnerRezApiUser) -> dict:
+def delete(request_url: furl, auth_user: orez.OwnerRezApiUser) -> dict:
+    with httpx.Client(headers=auth_user.headers()) as client:
+        r = client.delete(request_url.url, auth=auth_user.get_auth())
+        r.raise_for_status()
+        return r.json()
+
+
+async def delete_async(request_url: furl, auth_user: orez.OwnerRezApiUser) -> dict:
     async with httpx.AsyncClient(headers=auth_user.headers()) as client:
         r = await client.delete(request_url.url, auth=auth_user.get_auth())
         r.raise_for_status()
